c85264e7bfc9a51b8c4943486ac48574f657cb13
Omtatah <omtatahmurtallah@gmail.com>
2019-03-19 11:18:09 +0300

    my first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+undefined
+var number1 = parseInt(prompt("Enter a number:"));
+undefined
+var number2 = parseInt(prompt("Enter another number:"));
+undefined
+alert(add(number1, number2));
+undefined
